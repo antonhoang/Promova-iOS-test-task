49b6970b56dbc0433c2375a237d67552a274ebc2
Alina <macbook_alina@macbook-air-alina.home>
2024-01-19 14:39:11 +0100

    Initial commit

---
+++ Promova-iOS/Promova_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Promova_iOSApp.swift
+//  Promova-iOS
+//
+//  Created by Alina on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Promova_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

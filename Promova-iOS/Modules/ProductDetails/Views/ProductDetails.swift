343a9de2c1576212cb70790eef94a5842b0b0eff
antonhoang <macbook_alina@macbook-air-alina.home>
2024-01-20 12:30:38 +0100

    Create Products details

--- Promova-iOS/Modules/ProductDetails/Views/ProductDetails.swift
+++ Promova-iOS/Modules/ProductDetails/Views/ProductDetails.swift
@@ -11,7 +11,19 @@ import SwiftUI
 struct ProductDetails: View {
     let product: Product
     var body: some View {
-        Text(product.title)
+        ZStack {
+            Color(UIColor(hex: 0xBEC8FF))
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.white)
+                .frame(width: 335, height: 435, alignment: .center)
+                .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: 20)
+                .overlay {
+                    VStack {
+                        Image("BitmapFullSize")
+                    }
+                }
+                .padding()
+        }        
     }
 }
 

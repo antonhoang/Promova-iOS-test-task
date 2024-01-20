82def6f4a50373941ccd54ecd3070f6067842a48
antonhoang <macbook_alina@macbook-air-alina.home>
2024-01-20 12:44:03 +0100

    create UI for products details

--- Promova-iOS/Modules/ProductDetails/Views/ProductDetails.swift
+++ Promova-iOS/Modules/ProductDetails/Views/ProductDetails.swift
@@ -9,21 +9,44 @@
 import SwiftUI
 
 struct ProductDetails: View {
+//    @EnvironmentObject private var store: Store
+
     let product: Product
     var body: some View {
         ZStack {
             Color(UIColor(hex: 0xBEC8FF))
             RoundedRectangle(cornerRadius: 10)
                 .fill(Color.white)
-                .frame(width: 335, height: 435, alignment: .center)
+                .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height / 1.7, alignment: .center)
                 .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: 20)
                 .overlay {
                     VStack {
                         Image("BitmapFullSize")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .padding()
+                        Text("Fact text")
+                        Spacer()
+                        HStack {
+                            Button(action: {
+                                print("Button tapped!")
+                            }) {
+                                Image("back 1")
+                            }
+                            .padding()
+                            Spacer()
+                            Button(action: {
+                                print("Button tapped!")
+                            }) {
+                                Image("next 1")
+                            }
+                            .padding()
+                            
+                        }
                     }
                 }
                 .padding()
-        }        
+        }.ignoresSafeArea()
     }
 }
 
